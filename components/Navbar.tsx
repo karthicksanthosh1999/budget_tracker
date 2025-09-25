2afc1f0dc5601e279949c01f2f1bb7fd3bc98936
karthicksanthosh <karthick.santhosh1999@gmail.com>
2025-09-25 22:53:37 +0530

    first commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,136 @@
+"use client";
+import React, { useState } from "react";
+import Logo, { LogoMobile } from "./Logo";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { Button, buttonVariants } from "./ui/button";
+import { UserButton } from "@clerk/nextjs";
+import ThemeSwitcherBtn from "./ThemeSwitcherBtn";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
+
+const Navbar = () => {
+  return (
+    <>
+      <DesktopNavbar />
+      <MobileNavbar />
+    </>
+  );
+};
+
+const items = [
+  {
+    label: "Dashboard",
+    link: "/",
+  },
+  {
+    label: "Transactions",
+    link: "/transactions",
+  },
+  {
+    label: "Manage",
+    link: "/manage",
+  },
+  {
+    label: "About",
+    link: "/about",
+  },
+];
+
+function DesktopNavbar() {
+  return (
+    <div className="hidden border-separate border-b bg-background md:block">
+      <nav className="container flex items-center justify-between px-8">
+        <div className="flex h-[80px] min=h-[60px] items-center gap-x-4">
+          <Logo />
+          <div className="flex h-full">
+            {items &&
+              items.map((item) => (
+                <NavbarItem
+                  key={item.label}
+                  link={item.link}
+                  label={item.label}
+                />
+              ))}
+          </div>
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSwitchSessionUrl="/sign-in" />
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+function MobileNavbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <div className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger asChild>
+            <Button variant={"ghost"} size={"icon"}>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[280px] sm:w-[540px]" side="left">
+            <LogoMobile />
+            <div className="flex flex-col gap-1 pt-4">
+              {items.map((item) => (
+                <NavbarItem
+                  clickCallBack={() => setIsOpen((prev) => !prev)}
+                  key={item.label}
+                  label={item.label}
+                  link={item.link}
+                />
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <LogoMobile />
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSwitchSessionUrl="/sign-in" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function NavbarItem({
+  link,
+  label,
+  clickCallBack,
+}: {
+  link: string;
+  label: string;
+  clickCallBack?: () => void;
+}) {
+  const pathName = usePathname();
+  const isActive = pathName === link;
+  return (
+    <div className="relative flex items-center">
+      <Link
+        href={link}
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "w-full justify-start text-lg text-muted-foreground hover:text-foreground",
+          isActive && "text-foreground"
+        )}
+        onClick={() => {
+          if (clickCallBack) clickCallBack;
+        }}>
+        {label}
+      </Link>
+      {isActive && (
+        <div className="absolute -bottom-[2px] left-1/2 hidden h-[2px] w-[80%] -translate-1/2 rounded-xl bg-foreground md:block" />
+      )}
+    </div>
+  );
+}
+
+export default Navbar;
