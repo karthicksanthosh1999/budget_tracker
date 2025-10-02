0439ad1c71548b0c6e6e6c658f55ff306029f509
karthicksanthosh <karthick.santhosh1999@gmail.com>
2025-10-02 08:22:29 +0530

    Working on the filter function

--- app/(dashboard)/_components/StateCards.tsx
+++ app/(dashboard)/_components/StateCards.tsx
@@ -1,7 +1,11 @@
 "use client";
+import { GetBalanceStatusResponseType } from "@/app/api/stacks/balance/route";
+import SkeletonWrapper from "@/components/SkeletonWrapper";
 import { UserSettings } from "@/lib/generated/prisma";
+import { DateToUTCDate, GetFormatterForCurrency } from "@/lib/helper";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import { TrendingUp } from "lucide-react";
+import React, { ReactNode, useMemo } from "react";
 
 interface Props {
   from: Date;
@@ -10,8 +14,54 @@ interface Props {
 }
 
 const StateCards = ({ from, to, userSettings }: Props) => {
-  const statsQuery = useQuery();
-  return <div></div>;
+  const statsQuery = useQuery<GetBalanceStatusResponseType>({
+    queryKey: ["overview", "stats", from, to],
+    queryFn: () =>
+      fetch(
+        `/api/stats/balance?from=${DateToUTCDate(from)}&to=${DateToUTCDate(
+          to
+        )}}`
+      ).then((res) => res.json()),
+  });
+
+  const formatter = useMemo(() => {
+    return GetFormatterForCurrency(userSettings.currency);
+  }, [userSettings.currency]);
+
+  const income = statsQuery.data?.income || 0;
+  const expense = statsQuery.data?.expense || 0;
+
+  const balance = income - expense;
+
+  return (
+    <div className="relative flex w-full flex-wrap gap-2 md:flex-nowrap">
+      <SkeletonWrapper isLoading={statsQuery.isFetched}>
+        <StatCard
+          formatter={formater}
+          value={income}
+          title="Income"
+          icon={
+            <TrendingUp
+              className="h-12 w-12 items-center rounded-lg p-2 text-emerald-500 
+          bg-emerald-400/10"
+            />
+          }
+        />
+      </SkeletonWrapper>
+    </div>
+  );
 };
 
 export default StateCards;
+
+function StatCard({
+  formater,
+  value,
+  title,
+  icon,
+}: {
+  formatter: Intl.NumberFormat;
+  icon: ReactNode;
+  title: String;
+  value: number;
+}) {}
--- lib/helper.ts
+++ lib/helper.ts
@@ -1,3 +1,5 @@
+import { Currencies } from "./currency";
+
 export const DateToUTCDate = (date: Date) => {
   return new Date(
     Date.UTC(
@@ -11,3 +13,12 @@ export const DateToUTCDate = (date: Date) => {
     )
   );
 };
+
+export function GetFormatterForCurrency(currency: string) {
+  const locate = Currencies.find((c) => c.value === currency)?.local;
+
+  return new Intl.NumberFormat(locate, {
+    style: "currency",
+    currency,
+  });
+}
